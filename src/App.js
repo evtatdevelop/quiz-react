f04e5635e425ed8665aa926ffe23b23384898dc3
Evgenii <evtatdevelop@gmail.com>
2021-06-29 16:21:18 +0700

    add support of SASS & syle-modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import classes from './App.module.scss';
+console.log(classes);
 
 function App() {
   return (
-    <div className="App">
-      <h1>H! REACT</h1>
+    <div className={classes.App}>
+      <h1 className = {classes['header']}>H! REACT</h1>
     </div>
   );
 }
